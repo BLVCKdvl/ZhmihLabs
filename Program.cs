1a82e45adf122c651e8fa29f55287ccee0e4ed4e
Dmitry Makanov <dmakanov62@gmail.com>
2022-08-03 22:34:30 +0300

    Del Main defin

--- Program.cs
+++ Program.cs
@@ -1,21 +1,10 @@
-﻿using System;
+﻿
+List<Student> students = new List<Student>(){
+ new Student { FirstName = "Dmitry", LastName = "Makanov", Age = 20 },
+ new Student { FirstName = "Dmitry", LastName = "Korzhitsky", Age = 18 }
+};
 
-namespace Program
+foreach (var student in students)
 {
-    static class Program
-    {
-        static void Main()
-        {
-            Student[] students = new[]
-            {
-                new Student { FirstName = "Dmitry", LastName = "Makanov", Age = 20 },
-                new Student { FirstName = "Dmitry", LastName = "Korzhitsky", Age = 18 }
-            };
-
-            foreach (var student in students)
-            {
-                Console.WriteLine(student.ToString());
-            }
-        }
-    }
+    Console.WriteLine(student);
 }
