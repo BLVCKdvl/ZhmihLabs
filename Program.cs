e615c8a9c13047607063efce5f0ed97e280b5637
XD <s20.rex9000@gmail.com>
2022-08-03 22:04:22 +0300

    hello2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace Program
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Hello, my friend!");
+            Console.WriteLine("Hello2");
+        }
+    }
+}
